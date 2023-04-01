aeac21f10d18a5d0d89afdbc7215ca1a7ede02d9
dmtrack <rightrack888@gmail.com>
2023-04-01 22:51:53 +0500

    refactor: url4

--- src/app.ts
+++ src/app.ts
@@ -19,13 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use(indexRouter);
 
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
 export const server = http.createServer(app);
-app.use('/', indexRouter);
+app.use(indexRouter);
 
 export default app;
